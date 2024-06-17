6c64a5f09c9f9bfc618078dee6a8ad91f1e1c6b3
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-06-17 17:45:46 +0530

    Shopping List App

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -2,12 +2,15 @@ package com.example.shoppinglist
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,18 +21,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-data class SoppingItem(
+data class ShoppingItem(
     val id: Int,
     var name: String,
     var quantity: Int,
     var isEditing: Boolean = false
 )
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun ShoppingListApp(){
-    var sItems by remember { mutableStateOf(listOf<SoppingItem>()) }
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember{ mutableStateOf(false) }
+    var itemName by remember{ mutableStateOf("") }
+    var itemQuantity by remember{ mutableStateOf("") }
 
 
 
@@ -38,9 +43,7 @@ fun ShoppingListApp(){
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = {
-                // Your logic to add an item
-            },
+            onClick = {showDialog = true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Add Item")
@@ -57,9 +60,54 @@ fun ShoppingListApp(){
         }
     }
     if(showDialog){
-        AlertDialog(onDismissRequest = {showDialog = false}){
-            Text(text = "This is Alert Dialog !!")
-        }
+        AlertDialog(onDismissRequest = {showDialog = false},
+            confirmButton = {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                    Button(onClick = {
+                        if (itemName.isNotBlank()){
+                            val newItem = ShoppingItem(
+                                id = sItems.size+1,
+                                name = itemName,
+                                quantity = itemQuantity.toInt()
+                            )
+                            sItems = sItems + newItem
+                            showDialog = false
+                            itemName = ""
+                        }
+                    }) {
+                        Text(text = "Add")
+                    }
+                    Button(onClick = {showDialog = false}) {
+                        Text(text = "Cancel")
+                    }
+                }
+            },
+            title = { Text(text = "Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(value =itemName ,
+                        onValueChange = {itemName=it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp))
+
+                    OutlinedTextField(value =itemQuantity ,
+                        onValueChange = {itemQuantity=it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp))
+                }
+            })
     }
+
+}
+
+@Composable
+fun ShoppingListItem(){
 
 }
